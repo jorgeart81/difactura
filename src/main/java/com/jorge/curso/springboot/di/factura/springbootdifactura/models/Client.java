0d322a82c5c73cb672bb33a2d94c9b245baa76d5
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-06 16:14:12 -0500

    Create Client model

---
+++ src/main/java/com/jorge/curso/springboot/di/factura/springbootdifactura/models/Client.java
@@ -0,0 +1,22 @@
+package com.jorge.curso.springboot.di.factura.springbootdifactura.models;
+
+public class Client {
+  private String name;
+  private String lastname;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+}
