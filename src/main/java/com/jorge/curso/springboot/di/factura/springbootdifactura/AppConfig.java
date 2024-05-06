cbac0ad1a346d93001470e344f06dff95763f68b
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-06 17:01:07 -0500

    Create AppConfig

---
+++ src/main/java/com/jorge/curso/springboot/di/factura/springbootdifactura/AppConfig.java
@@ -0,0 +1,24 @@
+package com.jorge.curso.springboot.di.factura.springbootdifactura;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.jorge.curso.springboot.di.factura.springbootdifactura.models.Item;
+import com.jorge.curso.springboot.di.factura.springbootdifactura.models.Product;
+
+@Configuration
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+
+  @Bean
+  List<Item> itemsInvoice() {
+    return Arrays.asList(
+        new Item(new Product("Camara Sony", 800), 2),
+        new Item(new Product("Bicicleta", 1200), 4));
+  }
+
+}
